84600f4f2fc98c7063847be1a9029993d7715419
p1ho <hopokuan@gmail.com>
2018-01-23 21:01:52 -0500

    This is my second commit with 8ball.py

---
+++ 8ball.py
@@ -0,0 +1,26 @@
+import random
+answer_ls = ["It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Don't count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful"]
+input_ = input("Ask a question!\n")
+if input_[-1] != '?':
+    input_ += '?'
+print('You asked: ' + input_)
+print('Answer: ' + random.choice(answer_ls))
